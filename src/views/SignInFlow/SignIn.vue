6fffad390dbb41492a2dfc0758d734bb81ddbff9
Pippinhu <m18607273750@163.com>
2019-10-01 14:14:52 +0800

    update darkMode

--- src/views/SignInFlow/SignIn.vue
+++ src/views/SignInFlow/SignIn.vue
@@ -1,30 +1,101 @@
 <template>
-    <div class="container">
-        <div class="request">Don't have a design+code HQ account <router-link to="/request">Request an Account</router-link></div>
+    <div class="container" :class="{'light-background':!isDarkMode,'dark-background':isDarkMode}">
+        <div class="request" :class="{'light-request':isDarkMode,'dark-request':!isDarkMode}">Don't have a design+code HQ account <router-link to="/request">Request an Account</router-link></div>
         <div class="login">
-            <img src="@/assets/DCHQ.svg">
-            <h4>Sign into Design+Code HQ</h4>
-            <input type="email" placeholder="Email">
-            <input type="password" placeholder="Password">
+            <img src="@/assets/DCHQ.svg" v-if="isDarkMode">
+            <img src="@/assets/DCHQ-dark.svg" v-if="!isDarkMode">
+            <h4 :class="{'light-text':!isDarkMode,'dark-text':isDarkMode}">Sign into Design+Code HQ</h4>
+            <input :class="{'light-field':isDarkMode,'dark-field':!isDarkMode}" type="email" placeholder="Email">
+            <input :class="{'light-field':isDarkMode,'dark-field':!isDarkMode}" type="password" placeholder="Password">
             <button>Sign in</button>
-            <router-link to="/reover">forgot your password</router-link>
+            <router-link :class="{'light-request':isDarkMode,'dark-request':!isDarkMode}" to="/reover">forgot your password</router-link>
+            <button @click="toggleDarkMode">Toggle</button>
         </div>
-
     </div>
 </template>
 
 <script>
     export default {
-        name:"SingIn"
+        name:"SingIn",
+        computed: {
+            isDarkMode(){
+              return this.$store.getters.isDarkMode
+            }, 
+        },   
+        methods: {
+            toggleDarkMode(){
+                this.$store.commit("toggleDarkMode")
+            }
+        }
+        // computed: {
+        //   isDarkMode() {
+        //     return this.$store.getters.isDarkMode;
+        //   }
+        // },
+        // methods: {
+        //   toggleDarkMode() {
+        //   this.$store.commit("toggleDarkMode");
+        // }
+
     }
 </script>
 
 <style lang="scss" scoped>
+
+*{
+  transition: 0.8s,cubic-bezier(0.2, 0.8, 0.2, 1)
+}
+.light-background {
+  background-color: $light-gray;
+}
+
+.dark-background {
+  background-color: $dark-blue;
+}
+
+.light-text {
+  color: $black;
+}
+
+.dark-text {
+  color: $white;
+}
+
+.light-field {
+  background: rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.3);
+  }
+}
+
+.dark-field {
+  background: rgba(198, 208, 235, 0.2);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.3);
+  }
+}
+
+.light-request {
+  color: rgba(255, 255, 255, 0.3);
+
+  a {
+    color: white;
+  }
+}
+
+.dark-request {
+  color: rgba(0, 0, 0, 0.3);
+
+  a {
+    color: $black;
+  }
+}
 .container{
     display:flex;
     justify-content: center;
     align-items:center;
-
     min-height:100vh;
 }
 
@@ -36,10 +107,7 @@
     position:absolute;
     top:40px;
     right:40px;
-    color:rgba(255, 255, 255, 0.3);
-
-    a{
-        color:white;}
+    // color:rgba(255, 255, 255, 0.3);
 
 }
 
